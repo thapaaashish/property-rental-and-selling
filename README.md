e09c1e6e565b5061272e82f473bb8a86c59163fa
thapaaashish <np03cs4a220465@heraldcollege.edu.np>
2025-02-05 18:36:03 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# property-rental-and-selling" 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div class="bg-sky-200">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+export default App
