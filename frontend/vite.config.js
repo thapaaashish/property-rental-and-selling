e0bb8d2418d3c63a96d7042768aa48ae84be8560
thapaaashish <np03cs4a220465@heraldcollege.edu.np>
2025-04-28 17:13:39 +0545

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: `${import.meta.env.VITE_API_URL}`,
+        target: process.env.VITE_API_URL || "http://localhost:3000",
         secure: false,
         changeOrigin: true,
       },
